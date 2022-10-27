57b2aaccbbe01182f5be09fcfbcc47b8b5e4c3f1
Pianonic <79938743+Pianonic@users.noreply.github.com>
2022-10-27 08:16:35 +0200

    Visual Studio

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NahrungsnetzAuftrag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
